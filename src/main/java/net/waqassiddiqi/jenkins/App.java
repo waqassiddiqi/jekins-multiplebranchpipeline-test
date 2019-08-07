c7c5c323cda030ee5b6bb82946e6504183dc5656
Waqas Hussain <waqas.hussain@vroc.ai>
2019-08-07 09:52:07 +0800

    init commit

---
+++ src/main/java/net/waqassiddiqi/jenkins/App.java
@@ -0,0 +1,13 @@
+package net.waqassiddiqi.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
