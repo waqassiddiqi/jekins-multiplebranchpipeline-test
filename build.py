3929fdc2f7cd0b721a2ff6b223d6717815757185
Waqas Hussain <waqas.hussain@vroc.ai>
2019-09-19 15:49:31 +0800

    use throw error from python script

--- build.py
+++ build.py
@@ -2,6 +2,8 @@
 a=False
 b=True
 if(a):
-	raise Exception("a is true")
+	#raise Exception("a is true")
+	print("a")
 if(b):
-	raise Exception("b is true")
+	#raise Exception("b is true")
+	print("a")
